e82ae8b794232f8841e843498371e8af7e31eb62
Alvee <alvee@example.com>
2026-07-04 02:22:20 +0600

    Add sendMessage function to script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function showWelcome() {
     alert("Welcome to Alvee's Game Shop! Browse our products and enjoy gaming!");
 }
+
+function sendMessage() {
+    var name = document.getElementById("nameBox").value;
+    var email = document.getElementById("emailBox").value;
+    var msg = document.getElementById("msgBox").value;
+
+    if (name == "" || email == "" || msg == "") {
+        alert("Please fill in all fields before sending.");
+    }
+    else {
+        alert("Thank you " + name + "! Your message has been sent. We will reply to " + email + " soon.");
+        document.getElementById("nameBox").value = "";
+        document.getElementById("emailBox").value = "";
+        document.getElementById("msgBox").value = "";
+    }
+}
